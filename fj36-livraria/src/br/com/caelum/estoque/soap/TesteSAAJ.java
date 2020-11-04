b57cdd78a0fafa509c01b8b667cccef930eabcf8
RodrigoFrancaBR <rfrancacosta@gmail.com>
2020-11-04 00:24:01 -0200

    granularidade do servico

---
+++ fj36-livraria/src/br/com/caelum/estoque/soap/TesteSAAJ.java
@@ -0,0 +1,37 @@
+package br.com.caelum.estoque.soap;
+
+import java.io.IOException;
+
+import javax.xml.namespace.QName;
+import javax.xml.soap.MessageFactory;
+import javax.xml.soap.SOAPBody;
+import javax.xml.soap.SOAPBodyElement;
+import javax.xml.soap.SOAPElement;
+import javax.xml.soap.SOAPException;
+import javax.xml.soap.SOAPMessage;
+
+public class TesteSAAJ {
+
+	public static void main(String[] args) throws SOAPException, IOException {
+
+		MessageFactory factory = MessageFactory.newInstance();
+
+		SOAPMessage message = factory.createMessage();
+
+		SOAPBody body = message.getSOAPBody();
+
+		QName qualifiedName = new QName("http://ws.estoque.caelum.com.br/", "getQuantidade", "ns2");
+
+		SOAPBodyElement element = body.addBodyElement(qualifiedName);
+
+		SOAPElement codigoProduto = element.addChildElement(new QName("codigo"));
+
+		codigoProduto.setValue("ARQ");
+
+		SOAPElement quantidade = element.addChildElement(new QName("quantidade"));
+
+		quantidade.setValue("2");
+
+		message.writeTo(System.out);
+	}
+}
